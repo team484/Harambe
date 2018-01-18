553295f62d70ab4bc7c309653636b6dbf34fecbe
nyancatgrumpycat <34357831+nyancatgrumpycat@users.noreply.github.com>
2018-01-17 19:20:10 -0500

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -1,52 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package org.usfirst.frc.team484.robot;
 
 import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Joystick.AxisType;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.TalonSRX;
+//import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * creating this project, you must also update the build.properties file in the
+ * project.
  */
+public class Robot extends IterativeRobot {
+	private static final String kDefaultAuto = "Default";
+	private static final String kCustomAuto = "My Auto";
+	private String m_autoSelected; 
+	private SendableChooser<String> m_chooser = new SendableChooser<>();
+	public Talon leftSpeed = new Talon(0);
+	public Talon rightSpeed = new Talon(1);
+	public DifferentialDrive drive = new DifferentialDrive(leftSpeed, rightSpeed);
+	public AnalogInput ir = new AnalogInput(0);
 
-public class Robot extends IterativeRobot{
-	public static AnalogInput gearSensor;
-	int di =0;
-	int [] data = new int [10];
-			
-	
-	public static final int DRIVE_STICK_PORT = 1;
-	
-
-	// 0 -> climber
-
-	private Joystick driveStick;
-	private RobotDrive drive;
-	private TalonSRX rightMotor, leftMotor;
-	private Encoder leftEncoder, rightEncoder;
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
-		System.out.println("Ryan's RobotInit");
-		rightMotor = new TalonSRX(1);
-		leftMotor = new TalonSRX(0);
-		leftEncoder = new Encoder(0, 1);
-		rightEncoder = new Encoder(2,3);
-		
-		driveStick = new Joystick(DRIVE_STICK_PORT);
-		
-		drive = new RobotDrive(rightMotor, leftMotor);
-		gearSensor = new AnalogInput(02);
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
 	}
 
 	/**
@@ -56,53 +50,50 @@ public class Robot extends IterativeRobot{
 	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
 	 * getString line to get the auto name from the text box below the Gyro
 	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
 	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
 	@Override
 	public void autonomousInit() {
+		m_autoSelected = m_chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
+		// defaultAuto);
+		System.out.println("Auto selected: " + m_autoSelected);
 	}
 
 	/**
-	 * This function is called periodically during autonomous
+	 * This function is called periodically during autonomous.
 	 */
 	@Override
 	public void autonomousPeriodic() {
+		switch (m_autoSelected) {
+			case kCustomAuto:
+				// Put custom auto code here
+				break;
+			case kDefaultAuto:
+			default:
+				// Put default auto code here
+				break;
+		}
 	}
 
 	/**
-	 * This function is called periodically during operator control
+	 * This function is called periodically during operator control.
 	 */
 	@Override
 	public void teleopPeriodic() {
-		double move = driveStick.getAxis(AxisType.kY);
-		double rotate = -driveStick.getTwist();
-	
-		drive.arcadeDrive(0.7 * move, 0.7 * rotate);
-		
-		//System.out.println("IR sensor" + isGearIn());
-		data[di] = gearSensor.getAverageValue();
-		if (di == 10) {
-			di = 0;
+		if(ir.getAverageVoltage() > 10) {
+			drive.arcadeDrive(0.0, 0.0);
+		}else {
+			drive.arcadeDrive(0.4, 0);
 		}
-		int sum = 0;
-		for(int x =0; x < 10; x++ ) {
-			sum +=data[x];
-		}
-		System.out.println(sum);
 	}
 
- 
 	/**
-	@Override
-	 * This function is called periodically during test mode
+	 * This function is called periodically during test mode.
 	 */
+	@Override
 	public void testPeriodic() {
 	}
-
-	
-	
-	
-
 }
